c7a864c6b0a0635140baf062d44c1acfc36c915e
Jaime Andrés Arriagada Rosas <163452202+JaimeArriagadaRosas@users.noreply.github.com>
2026-09-24 17:08:32 -0300

    feat(data): add dataset adapter package

---
+++ src/lithiumscope/datasets/adapters/__init__.py
@@ -0,0 +1,9 @@
+from lithiumscope.datasets.adapters.georoc import (
+    GeorocAdapterConfig,
+    harmonize_georoc_frame,
+)
+
+__all__ = [
+    "GeorocAdapterConfig",
+    "harmonize_georoc_frame",
+]
